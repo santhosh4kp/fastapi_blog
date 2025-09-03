0daed7e7d6f08af0ca283adf545e7bdbf569c6be
Santhosh Kumar K P <santhosh4kp@gmail.com>
2025-09-03 09:11:17 +0530

    initial commit

---
+++ backend/core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    PROJECT_TITLE: str="Blog🚀"
+    PROJECT_VERSION: str ="0.1.0"
+
+settings=Settings()
---
+++ backend/main.py
@@ -0,0 +1,11 @@
+#main.py
+
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+
+
+@app.get("/")
+def hello_api():
+    return {"msg":"Hello FastAPI🚀"}
